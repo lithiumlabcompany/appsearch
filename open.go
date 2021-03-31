b71137cee3c4d79135c48b0ac48e7fde88267832
Yury Chirkov <yo@yo.qa>
2021-03-31 14:48:35 +0300

    Allow opening client with basic auth

--- open.go
+++ open.go
@@ -1,6 +1,8 @@
 package appsearch
 
 import (
+	"encoding/base64"
+	"fmt"
 	"net/url"
 
 	"github.com/go-resty/resty/v2"
@@ -11,36 +13,47 @@ import (
 // https://private-...@abcd.ent-search.eu-central-1.aws.cloud.es.io
 // Second parameter is always interpreted as API key if specified
 func Open(endpointAndKey ...string) (*client, error) {
-	hostURL, key, err := getHostURL(endpointAndKey)
+	hostURL, token, authType, err := getHostURL(endpointAndKey)
 
 	return &client{
 		resty.New().
 			SetHostURL(hostURL).
-			SetAuthToken(key).
-			SetAuthScheme("Bearer"),
+			SetAuthToken(token).
+			SetAuthScheme(authType),
 	}, err
 }
 
-func getHostURL(params []string) (hostUrl string, key string, err error) {
+func getHostURL(params []string) (hostUrl string, token string, authType string, err error) {
 	switch len(params) {
 	case 2:
-		hostUrl, _, err = resolve(params[0])
-		key = params[1]
-		return hostUrl, key, err
+		hostUrl, _, _, err = resolve(params[0])
+		authType = "Bearer"
+		token = params[1]
+		return
 	case 1:
 		return resolve(params[0])
 	default:
-		return "", "", ErrInvalidParams
+		return "", "", "", ErrInvalidParams
 	}
 }
 
-func resolve(rawUrl string) (hostUrl string, key string, err error) {
+func resolve(rawUrl string) (hostUrl string, token string, authType string, err error) {
 	u, err := url.Parse(rawUrl)
 	if err != nil {
 		return
 	}
 	if u.User != nil {
-		key = u.User.String()
+		username := u.User.Username()
+		password, isBasicAuth := u.User.Password()
+		if isBasicAuth {
+			authType = "Basic"
+			token = base64.StdEncoding.EncodeToString([]byte(
+				fmt.Sprintf("%s:%s", u.User.Username(), password),
+			))
+		} else {
+			authType = "Bearer"
+			token = username
+		}
 		u.User = nil
 	}
 	hostUrl = u.ResolveReference(&url.URL{Path: "/api/as/v1/"}).String()
--- open_test.go
+++ open_test.go
@@ -8,60 +8,47 @@ import (
 
 func TestOpen(t *testing.T) {
 	t.Run("resolve", func(t *testing.T) {
-		t.Run("Must resolve host with key", func(t *testing.T) {
-			host, key, err := resolve("https://key@host")
+		t.Run("Must resolve host with token", func(t *testing.T) {
+			host, token, authType, err := resolve("https://token@host")
 			require.NoError(t, err)
 			require.Equal(t, "https://host/api/as/v1/", host)
-			require.Equal(t, "key", key)
+			require.Equal(t, "token", token)
+			require.Equal(t, "Bearer", authType)
 		})
 
-		t.Run("Must resolve host without key", func(t *testing.T) {
-			host, key, err := resolve("https://host")
+		t.Run("Must resolve host with basic auth", func(t *testing.T) {
+			host, token, authType, err := resolve("https://uname:pass@host")
 			require.NoError(t, err)
 			require.Equal(t, "https://host/api/as/v1/", host)
-			require.Equal(t, "", key)
+			require.Equal(t, "token", token)
+			require.Equal(t, "Basic", authType)
+		})
+
+		t.Run("Must resolve host without token", func(t *testing.T) {
+			host, _, _, err := resolve("https://host")
+			require.NoError(t, err)
+			require.Equal(t, "https://host/api/as/v1/", host)
 		})
 
 		t.Run("Must return error for invalid URL", func(t *testing.T) {
-			_, _, err := resolve("%")
+			_, _, _, err := resolve("%")
 			require.Error(t, err)
-		})
-	})
-
-	t.Run("getHostURL", func(t *testing.T) {
-		t.Run("Must resolve endpoint with auth", func(t *testing.T) {
-			host, key, err := getHostURL([]string{"https://key@host"})
-			require.NoError(t, err)
-			require.Equal(t, "https://host/api/as/v1/", host)
-			require.Equal(t, "key", key)
-		})
-
-		t.Run("Must resolve endpoint with key", func(t *testing.T) {
-			host, key, err := getHostURL([]string{"https://host", "key"})
-			require.NoError(t, err)
-			require.Equal(t, "https://host/api/as/v1/", host)
-			require.Equal(t, "key", key)
-		})
-
-		t.Run("Must return error for invalid URL", func(t *testing.T) {
-			_, _, err := getHostURL([]string{"%"})
-			require.Error(t, err)
-		})
-
-		t.Run("Must return error for invalid usage", func(t *testing.T) {
-			_, _, err := getHostURL([]string{})
-			require.ErrorIs(t, err, ErrInvalidParams)
 		})
 	})
 
 	t.Run("Open", func(t *testing.T) {
 		t.Run("Must open client from endpoint", func(t *testing.T) {
-			_, err := Open("https://key@host")
+			_, err := Open("https://token@host")
 			require.NoError(t, err)
 		})
 
-		t.Run("Must open client from endpoint with key", func(t *testing.T) {
-			_, err := Open("https://host", "key")
+		t.Run("Must open client from basic auth", func(t *testing.T) {
+			_, err := Open("https://uname:pass@host")
+			require.NoError(t, err)
+		})
+
+		t.Run("Must open client from endpoint with token", func(t *testing.T) {
+			_, err := Open("https://host", "token")
 			require.NoError(t, err)
 		})
 	})
